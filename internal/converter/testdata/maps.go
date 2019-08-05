796b52f0d21e25373a0f1d173807e50667692fb1
Chris (CruftMaster) <chrusty@gmail.com>
2019-08-05 21:49:42 +0100

    Updated unit test for maps

--- internal/converter/testdata/maps.go
+++ internal/converter/testdata/maps.go
@@ -2,64 +2,68 @@ package testdata
 
 const Maps = `{
     "$schema": "http://json-schema.org/draft-04/schema#",
-    "map_of_strings": {
-		"type": "object",
-		"additionalProperties": {
-			"type": "string"
-		}
-	},
-    "map_of_ints": {
-		"type": "object",
-		"additionalProperties": {
-			"type": "integer"
-		}
-	},
-	"map_of_messages": {
-		"type": "object",
-		"additionalProperties": {
-			"properties": {
-				"complete": {
-					"type": "boolean"
-				},
-				"id": {
-					"type": "integer"
-				},
-				"name": {
-					"type": "string"
-				},
-				"rating": {
-					"type": "number"
-				},
-				"timestamp": {
-					"type": "string"
-				},
-				"topology": {
-					"enum": [
-						"FLAT",
-						0,
-						"NESTED_OBJECT",
-						1,
-						"NESTED_MESSAGE",
-						2,
-						"ARRAY_OF_TYPE",
-						3,
-						"ARRAY_OF_OBJECT",
-						4,
-						"ARRAY_OF_MESSAGE",
-						5
-					],
-					"oneOf": [
-						{
-							"type": "string"
-						},
-						{
-							"type": "integer"
-						}
-					]
-				}
-			},
-			"additionalProperties": true,
-			"type": "object"
-		}
-	}
+    "properties": {
+        "map_of_ints": {
+            "additionalProperties": {
+                "type": "integer"
+            },
+            "type": "object"
+        },
+        "map_of_messages": {
+            "additionalProperties": {
+                "properties": {
+                    "complete": {
+                        "type": "boolean"
+                    },
+                    "id": {
+                        "type": "integer"
+                    },
+                    "name": {
+                        "type": "string"
+                    },
+                    "rating": {
+                        "type": "number"
+                    },
+                    "timestamp": {
+                        "type": "string"
+                    },
+                    "topology": {
+                        "enum": [
+                            "FLAT",
+                            0,
+                            "NESTED_OBJECT",
+                            1,
+                            "NESTED_MESSAGE",
+                            2,
+                            "ARRAY_OF_TYPE",
+                            3,
+                            "ARRAY_OF_OBJECT",
+                            4,
+                            "ARRAY_OF_MESSAGE",
+                            5
+                        ],
+                        "oneOf": [
+                            {
+                                "type": "string"
+                            },
+                            {
+                                "type": "integer"
+                            }
+                        ]
+                    }
+                },
+                "additionalProperties": true,
+                "type": "object"
+            },
+            "type": "object"
+        },
+        "map_of_strings": {
+            "additionalProperties": {
+                "type": "string"
+            },
+            "type": "object"
+        }
+    },
+    "additionalProperties": true,
+    "type": "object"
 }`
