b67f10545f30fcc281e38a046ac4fd03a70c643f
Chrusty <Chris@H>
2020-11-26 13:24:21 +1300

    This test was incorrect (schema shouldn't have allowed nulls)

--- internal/converter/testdata/google_value.go
+++ internal/converter/testdata/google_value.go
@@ -4,14 +4,8 @@ const GoogleValue = `{
     "$schema": "http://json-schema.org/draft-04/schema#",
     "properties": {
         "arg": {
-            "oneOf": [
-                {
-                    "type": "null"
-                },
-                {
-                    "type": "object"
-                }
-            ]
+            "additionalProperties": true,
+            "type": "object"
         }
     },
     "additionalProperties": true,
