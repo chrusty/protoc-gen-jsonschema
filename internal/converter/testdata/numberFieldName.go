a59e5a3254597c21bb7d4afaa910a426cd5bbf83
Grant Timmerman <timmerman+devrel@google.com>
2021-07-06 17:45:54 -0600

    feat: add well known type duration
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

---
+++ internal/converter/testdata/numberFieldName.go
@@ -0,0 +1,16 @@
+package testdata
+
+const NumberFieldName = `{
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "properties": {
+        "crc32c": {
+            "type": "string"
+        },
+        "timestamp": {
+            "type": "string"
+        }
+    },
+    "additionalProperties": true,
+    "type": "object"
+}
+`
