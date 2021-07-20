cd4ff2f197c77b367ad30891c21d4ba94ff17600
Prawn <chrusty@users.noreply.github.com>
2021-07-20 20:21:26 +1200

    Format and pattern (regex) for duration (#67)
    * Format and pattern (regex) for duration
    
    * We only need seconds (not other time units)
    
    Co-authored-by: Chrusty <>
    Co-authored-by: chris <chris@Profanity.local>

--- internal/converter/testdata/wellknown.go
+++ internal/converter/testdata/wellknown.go
@@ -28,9 +28,10 @@ const WellKnown = `{
             "type": "array"
         },
         "duration": {
-            "additionalProperties": true,
+            "pattern": "^([0-9]+\\.?[0-9]*|\\.[0-9]+)s$",
             "type": "string",
-            "description": "This is a duration:"
+            "description": "This is a duration:",
+            "format": "regex"
         }
     },
     "additionalProperties": true,
