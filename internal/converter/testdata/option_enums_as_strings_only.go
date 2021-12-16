9753ec15d4b92a8536a6f655323801a5b647fcaa
Mariusz Różycki <mariusz.rozycki@tink.com>
2021-12-16 17:11:25 +0100

    Add a flag to only generate strings in enums

---
+++ internal/converter/testdata/option_enums_as_strings_only.go
@@ -0,0 +1,15 @@
+package testdata
+
+const OptionEnumsAsStringsOnly = `{
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "enum": [
+        "NOT_SPECIFIED",
+        "USD",
+        "GBP",
+        "EUR"
+    ],
+    "type": "string"
+}`
+
+const OptionEnumsAsStringsOnlyPass = `"NOT_SPECIFIED"`
+const OptionEnumsAsStringsOnlyFail = `2`
