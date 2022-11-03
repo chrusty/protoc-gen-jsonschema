25bb9ddd239ebf3ec77de1ff54673e3b08fa888c
Rami Manna <rami@applied.co>
2022-11-03 14:58:39 -0700

    revert testdata change and unskip test

--- internal/converter/testdata/google_value.go
+++ internal/converter/testdata/google_value.go
@@ -25,7 +25,7 @@ const GoogleValue = `{
                         }
                     ],
                     "title": "Value",
-                    "description": "` + "`Value` represents a dynamically typed value which can be either null, a number, a string, a boolean, a recursive struct value, or a list of values. A producer of value is expected to set one of that variants. absence of any variant indicates an error. The JSON representation for `Value` is JSON value." + `
+                    "description": "` + "`Value`" + ` represents a dynamically typed value which can be either null, a number, a string, a boolean, a recursive struct value, or a list of values. A producer of value is expected to set one of these variants. Absence of any variant indicates an error. The JSON representation for ` + "`Value`" + ` is JSON value."
                 }
             },
             "additionalProperties": true,
